f1078480f8e0d8d51749b58363a6439f01318f59
roro1337 <damien.concept@gmail.com>
2016-03-23 14:21:27 +0100

    modif damien

--- CodilityCyclicRotation/src/vdd/cyclicrotation/CyclicRotation.java
+++ CodilityCyclicRotation/src/vdd/cyclicrotation/CyclicRotation.java
@@ -2,17 +2,9 @@ package vdd.cyclicrotation;
 
 /**
  * 
- * @author philippe
- *Allangros = Allan Gros
- *VDD PH
- *salut FrVDD Yoloooo
- *modif de cedric
- *modif de Adeline
- *modif d'Amandine
- *modif de Floriane
- *modif de Alexia
- *modif de MAthias
- *modif de Patick
+ * @author philippe Allangros = Allan Gros VDD PH salut FrVDD Yoloooo modif de
+ *         cedric modif de Adeline modif d'Amandine modif de Floriane modif de
+ *         Alexia modif de MAthias modif de Patick modif damien
  */
 public class CyclicRotation {
 	public static void main(String[] args) {
@@ -21,20 +13,20 @@ public class CyclicRotation {
 	}
 
 	private void init() {
-		int[] a={3, 8, 9, 7, 6};
+		int[] a = { 3, 8, 9, 7, 6 };
 		affTab(solution(a, 3));
 	}
 
 	public int[] solution(int[] A, int K) {
-		
+
 		return null;
 	}
-	
-	public void affTab(int[] t){
+
+	public void affTab(int[] t) {
 		System.out.print("[");
 		for (int i = 0; i < t.length; i++) {
 			System.out.print(t[i]);
-			if (i < t.length-1) {
+			if (i < t.length - 1) {
 				System.out.print(" ,");
 			}
 		}
