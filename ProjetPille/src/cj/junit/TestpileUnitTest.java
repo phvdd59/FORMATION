e8a9f3dcb9508c94eaab7414ade9d2717e6046a2
active_admin <active_admin@192.168.200.227>
2016-03-30 12:44:47 +0200

    modif vdd

--- ProjetPille/src/cj/junit/TestpileUnitTest.java
+++ ProjetPille/src/cj/junit/TestpileUnitTest.java
@@ -1,8 +1,11 @@
 package cj.junit;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import dd.metier.Pile;
 
 public class TestpileUnitTest {
 
