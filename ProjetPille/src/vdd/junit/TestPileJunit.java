109a2c53182b30e19c99ec155175771be98e1e38
phvdd59 <philippe.vandendriessche59@gmail.com>
2016-03-30 17:03:21 +0200

    test vide

--- ProjetPille/src/vdd/junit/TestPileJunit.java
+++ ProjetPille/src/vdd/junit/TestPileJunit.java
@@ -1,14 +1,10 @@
 package vdd.junit;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestPileJunit {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
 	}
-
 }
