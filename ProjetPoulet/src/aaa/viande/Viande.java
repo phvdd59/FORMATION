f794f54c30f2d023c1cb246a7e78f9b7f521ceca
Amandine JULLIEN <Amandine JULLIEN@192.168.200.37>
2016-04-08 11:02:32 +0200

    viande

--- ProjetPoulet/src/aaa/viande/Viande.java
+++ ProjetPoulet/src/aaa/viande/Viande.java
@@ -1,5 +1,53 @@
 package aaa.viande;
 
-public class Viande extends vdd.metier.Alimentaire {
+public class Viande extends vdd.metier.Alimentaire implements Volaille {
+
+	private String etat = "frigo";
+
+	public Viande() {
+		super();
+	}
+
+	@Override
+	public void deplumage() {
+		etat = "deplumage";
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		etat = "deplumer";
+
+	}
+
+	@Override
+	public void lavage() {
+		etat = "sale";
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		etat = "propre";
+
+	}
+
+	@Override
+	public boolean estDeplume() {
+		boolean plume = false;
+		if (etat == "deplumé") {
+			plume = true;
+		}
+		return plume;
+	}
+
+	@Override
+	public boolean estLave() {
+		boolean lave = false;
+		if (etat == "propre") {
+			lave = true;
+		}
+		return lave;
+	}
 
 }
