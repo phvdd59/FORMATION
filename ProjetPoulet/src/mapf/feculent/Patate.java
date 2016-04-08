666c89cc95bc9e69a035ab27f49b88759afe145d
Mathias <Mathias@192.168.200.148>
2016-04-08 11:23:31 +0200

    modif patate

--- ProjetPoulet/src/mapf/feculent/Patate.java
+++ ProjetPoulet/src/mapf/feculent/Patate.java
@@ -1,28 +1,40 @@
 package mapf.feculent;
 
 public class Patate extends Feculent implements Preparer {
-	
+
 	private String etatCuisson;
 	private String etatPreparation;
-	private String etatPropete = "sale";
-	
+	private String etatProprete = "sale";
+
 	public Patate() {
 		super();
 	}
-	
-	public Patate(String etatCuisson, String etatPreparation){
+
+	public Patate(String etatCuisson, String etatPreparation) {
 		this.etatCuisson = etatCuisson;
 		this.etatPreparation = etatPreparation;
 	}
 
 	@Override
 	public void nettoyer() {
-		
+		etatProprete = "sale";
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		etatProprete = "propre";
 	}
 
 	@Override
 	public void eplucher() {
-		
+		etatPreparation = "habillee";
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		etatPreparation = "epluchee";
 	}
 
 }
