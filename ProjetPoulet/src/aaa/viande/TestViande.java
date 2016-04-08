db492717a9264ca802de3d0715f47fdd4f6d14f8
Allan Gros <allan.gros@itech.fr>
2016-04-08 14:55:26 +0200

    junit

--- ProjetPoulet/src/aaa/viande/TestViande.java
+++ ProjetPoulet/src/aaa/viande/TestViande.java
@@ -1,21 +1,55 @@
 package aaa.viande;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestViande {
 	Volaille volaille;
+	Four four;
 
 	@Before
 	public void before() {
 		volaille = new Volaille();
+		four=new Four();
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testDeplumer() {
+		// fail("Not yet implemented");
+		volaille.deplumage();
+		String etat = volaille.getEtatVolaille();
+		assertEquals("deplumer", etat);
+	}
+
+	@Test
+	public void testLavage() {
+		volaille.lavage();
+		String etat = volaille.getEtatVolaille();
+		assertEquals("propre", etat);
+	}
+
+	@Test
+	public void testDecoupe() {
+		volaille.decoupe();
+		String etat = volaille.getEtatVolaille();
+		assertEquals("decoupé", etat);
+	}
+
+	@Test
+	public void testEstDecoupe() {
+		boolean b=false;
+		volaille.setEtatVolaille("decoupé");
+		b=volaille.estDecoupe();
+		assertTrue(b);
+	}
+	@Test 
+	public void testPrechauffer(){
+		four.prechauffer();
+		String etat=four.getEtatDuFour();
+		assertEquals("Prechauffe",etat);
 	}
 
 }
