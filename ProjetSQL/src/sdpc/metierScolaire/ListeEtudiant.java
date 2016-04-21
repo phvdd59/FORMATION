9e9170183423a66e592edbd372c3b560d8c5b7cb
Patrick W <Patrick W@192.168.200.218>
2016-04-21 16:56:59 +0200

    main methode scolaire

---
+++ ProjetSQL/src/sdpc/metierScolaire/ListeEtudiant.java
@@ -0,0 +1,13 @@
+package sdpc.metierScolaire;
+
+import java.util.ArrayList;
+
+public class ListeEtudiant extends ArrayList<Etudiant> {
+	
+	private Formation formation;
+	
+	public ListeEtudiant (){
+		this.formation=formation;
+	}
+
+}
---
+++ ProjetSQL/src/sdpc/metierScolaire/ListeFormationScolaire.java
@@ -0,0 +1,18 @@
+package sdpc.metierScolaire;
+
+import java.util.ArrayList;
+
+public class ListeFormationScolaire extends ArrayList<FormationScolaire> {
+
+	private ListeEtudiant listeEtudiant;
+
+	public ListeFormationScolaire() {
+		this.listeEtudiant = listeEtudiant;
+
+	}
+
+	public ListeFormationScolaire(ListeEtudiant listeEtudiant) {
+		this.listeEtudiant = listeEtudiant;
+
+	}
+}
