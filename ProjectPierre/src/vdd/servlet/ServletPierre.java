40c60c759c4f2ece64cc21cfe8cb52c3855f7617
Pierre Vandendriessche <vandendriessche.pierre@gmail.com>
2016-04-22 22:55:46 +0200

    ok...

--- ProjectPierre/src/vdd/servlet/ServletPierre.java
+++ ProjectPierre/src/vdd/servlet/ServletPierre.java
@@ -28,7 +28,7 @@ public class ServletPierre extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/* ZUT */
+		/* moi pas un roman */
 		File file = new File("../workspace/ProjectPierre/WebContent/pierre/pierre.txt");
 		if (file.exists()) {
 			System.out.println(file.getAbsolutePath());
