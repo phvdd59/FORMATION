87bec2918faf8e797c3893d571304bd7847bbde7
Mathias <Mathias@192.168.200.148>
2016-04-25 11:43:09 +0200

    Modif SevletListeEtudiant

--- ProjectCV/src/vdd/servlet/ServletListeEtudiant.java
+++ ProjectCV/src/vdd/servlet/ServletListeEtudiant.java
@@ -1,5 +1,14 @@
 package vdd.servlet;
 
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Servlet implementation class ServletListeEtudiant
  */
@@ -15,11 +24,9 @@ public class ServletListeEtudiant extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Enumeration<String> par = request.getParameterNames();
 		while (par.hasMoreElements()) {
 			String sVal = par.nextElement();
@@ -28,11 +35,9 @@ public class ServletListeEtudiant extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
 
